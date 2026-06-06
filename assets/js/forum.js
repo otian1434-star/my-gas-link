f4088c4df90cd169254f63d8e8fbf5e04f9dcba7
otian1434-star <otian1434-star@users.noreply.github.com>
2026-06-07 00:45:05 +0800

    Add infinite battle feature guide

--- assets/js/forum.js
+++ assets/js/forum.js
@@ -89,6 +89,7 @@
             <a href="${r}pages/features-newbie-guide.html">📚 新手引導</a>
             <a href="${r}pages/features-daily-quest.html">📜 每日任務</a>
             <a href="${r}pages/features-event-coin.html">🎉 活動金幣</a>
+            <a href="${r}pages/features-infinite-battle.html">⚔️ 無限大戰</a>
             <a href="${r}pages/features-jobs.html">⚔️ 職業之力</a>
             <a href="${r}pages/features-job-skills.html">📚 職業特色技能</a>
             <a href="${r}pages/features-mage-summon.html">✨ 法師召喚系統</a>
@@ -192,6 +193,7 @@
       <a class="mob-link mob-sub" href="${r}pages/features-newbie-guide.html">📚 新手引導</a>
       <a class="mob-link mob-sub" href="${r}pages/features-daily-quest.html">📜 每日任務</a>
       <a class="mob-link mob-sub" href="${r}pages/features-event-coin.html">🎉 活動金幣</a>
+      <a class="mob-link mob-sub" href="${r}pages/features-infinite-battle.html">⚔️ 無限大戰</a>
       <a class="mob-link mob-sub" href="${r}pages/features-jobs.html">⚔️ 職業之力</a>
       <a class="mob-link mob-sub" href="${r}pages/features-job-skills.html">📚 職業特色技能</a>
       <a class="mob-link mob-sub" href="${r}pages/features-mage-summon.html">✨ 法師召喚系統</a>
