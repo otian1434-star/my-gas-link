9a7acdaf8489d243b463018e471aaad26aee5e87
otian1434-star <otian1434-star@users.noreply.github.com>
2026-06-10 13:43:24 +0800

    Add snapper ring armor guide

--- assets/js/forum.js
+++ assets/js/forum.js
@@ -110,6 +110,7 @@
             <a href="${r}pages/armor-overview.html">🛡️ 防具介紹</a>
             <a href="${r}pages/armor-database.html">🔎 防具庫查詢</a>
             <a href="${r}pages/armor-enhance-protect.html">📜 防具強化與防爆</a>
+            <a href="${r}pages/armor-snappers-ring.html">💍 史奈普戒指</a>
             <a href="${r}pages/armor-special-shield.html">🛡️ 特殊臂盾介紹</a>
             <a href="${r}pages/items-overview.html">🎒 道具列表</a>
             <div class="nav-dd-divider"></div>
@@ -207,6 +208,7 @@
       <a class="mob-link mob-sub" href="${r}pages/armor-overview.html">🛡️ 防具介紹</a>
       <a class="mob-link mob-sub" href="${r}pages/armor-database.html">🔎 防具庫查詢</a>
       <a class="mob-link mob-sub" href="${r}pages/armor-enhance-protect.html">📜 防具強化與防爆</a>
+      <a class="mob-link mob-sub" href="${r}pages/armor-snappers-ring.html">💍 史奈普戒指</a>
       <a class="mob-link mob-sub" href="${r}pages/armor-special-shield.html">🛡️ 特殊臂盾介紹</a>
       <a class="mob-link mob-sub" href="${r}pages/items-overview.html">🎒 道具列表</a>
       <a class="mob-link mob-sub" href="${r}pages/items-dragon-eye.html">👁️ 四龍之魔眼</a>
