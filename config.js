678dc6b0b5ee29c17f341e76db6cd735dd1b314a
otian1434-star <otian1434-star@users.noreply.github.com>
2026-07-11 21:40:57 +0800

    Update game download links

--- config.js
+++ config.js
@@ -22,12 +22,12 @@ const FORUM_CONFIG = {
 
   // ── 遊戲下載連結（換論壇時更換）──────────────────────────────
   download: {
-    mainUrl:      "https://drive.google.com/file/d/1EvJ8gctj9W1Qd5p8qtibt8akQU0eRdJO/view?usp=drive_link",
-    backup1:      "https://drive.google.com/file/d/1I93hGQQ-zGJA3WljMhXbbQESx5PgmWT1/view?usp=drive_link",   // RAR
-    backup2:      "https://drive.google.com/file/d/1dUptGvBz3w3GlNxnfMtSJc_z4_j-9e4I/view?usp=sharing",       // 7Z
-    backup3:      "https://drive.google.com/file/d/1EvJ8gctj9W1Qd5p8qtibt8akQU0eRdJO/view?usp=drive_link",   // ZIP
+    mainUrl:      "https://drive.google.com/file/d/1vf8CFDjCYwOt4E41xTKE8tfO1-TB6dDg/view?usp=sharing",
+    backup1:      "https://drive.google.com/file/d/1SWtjnQK8a5o-c8DUYm3jGcjzy2Qg2uGC/view?usp=sharing",   // RAR
+    backup2:      "https://drive.google.com/file/d/1s9xne_bRHwwOFc5X2jGphjBtVfOckxgd/view?usp=sharing",   // 7Z
+    backup3:      "https://drive.google.com/file/d/1vf8CFDjCYwOt4E41xTKE8tfO1-TB6dDg/view?usp=sharing",   // ZIP
     patchUrl:     "",   // 合併補丁
-    updateDate:   "2026/07/08",
+    updateDate:   "2026/07/11",
     anyDeskUrl:   "https://anydesk.com/zh-tw/downloads/windows",
   },
 
